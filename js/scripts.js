3d8de63c4200ddc47e09487e14f46b1f4ec02bb0
sezj <sarahzachrich@gmail.com>
2020-04-30 06:44:56 -0400

    Close nav menu when main-content is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,6 @@
-let mainNavLinks = document.querySelectorAll("nav ul li a");
-let mainSections = document.querySelectorAll("main section");
-
-let lastId;
-let cur = [];
-
-// This should probably be throttled.
-// Especially because it triggers during smooth scrolling.
-// https://lodash.com/docs/4.17.10#throttle
-// You could do like...
-// window.addEventListener("scroll", () => {
-//    _.throttle(doThatStuff, 100);
-// });
-// Only not doing it here to keep this Pen dependency-free.
-
-window.addEventListener("scroll", event => {
-  let fromTop = window.scrollY;
-
-  mainNavLinks.forEach(link => {
-    let section = document.querySelector(link.hash);
-
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
-      link.classList.add("current");
-    } else {
-      link.classList.remove("current");
-    }
-  });
-});
+function retractMenu() {
+  document.getElementById("menu-btn").checked = false;
+}
+window.onload = function() {
+  document.getElementById("main-content").addEventListener('click', retractMenu);
+}
